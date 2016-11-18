0466fbb3f586cd54b7c16f1f5c1f41bb9de93e54
paulofernando <pf@paulofernando.net.br>
2016-11-18 17:44:52 -0300

    Adding a Fragment and RecyclerView to show the list of people. (in development)

--- PessoasInspiradoras/app/src/main/java/br/net/paulofernando/pessoasinspiradoras/view/activity/DashboardActivity.java
+++ PessoasInspiradoras/app/src/main/java/br/net/paulofernando/pessoasinspiradoras/view/activity/DashboardActivity.java
@@ -1,22 +1,20 @@
 package br.net.paulofernando.pessoasinspiradoras.view.activity;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
-import android.database.Cursor;
-import android.net.Uri;
+import android.database.SQLException;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.LinearLayout;
 
 import com.j256.ormlite.dao.Dao;
 
-import java.sql.SQLException;
 import java.util.List;
 
 import br.net.paulofernando.pessoasinspiradoras.R;
@@ -25,14 +23,14 @@ import br.net.paulofernando.pessoasinspiradoras.data.dao.DatabaseHelper;
 import br.net.paulofernando.pessoasinspiradoras.data.dao.DtoFactory;
 import br.net.paulofernando.pessoasinspiradoras.data.entity.Person;
 import br.net.paulofernando.pessoasinspiradoras.util.Utils;
+import br.net.paulofernando.pessoasinspiradoras.view.fragment.PersonListFragment;
 import br.net.paulofernando.pessoasinspiradoras.view.widget.PersonView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class DashboardActivity extends AppCompatActivity {
 
-    @BindView(R.id.layout_dashboard) LinearLayout dashboard;
-    @BindView(R.id.no_inspiration) LinearLayout noInspiration;
+    //@BindView(R.id.no_inspiration) LinearLayout noInspiration;
     @BindView(R.id.toolbar) Toolbar toolbar;
 
     private DtoFactory dtoFactory;
@@ -46,10 +44,9 @@ public class DashboardActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         dtoFactory = (DtoFactory) getApplication();
-        loadPersons();
     }
 
-    private void loadPersons() {
+    /*private void loadPersons() {
         dashboard.removeAllViews();
         DatabaseHelper helper = new DatabaseHelper(this);
         addPersons(helper.getPersonsData());
@@ -60,44 +57,11 @@ public class DashboardActivity extends AppCompatActivity {
         } else {
             noInspiration.setVisibility(View.VISIBLE);
         }
-    }
+    }*/
 
     void addPerson() {
         Intent intent = new Intent(this, AddPersonActivity.class);
         startActivity(intent);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (data != null) {
-            Uri uri = data.getData();
-
-            if (uri != null) {
-                Cursor c = null;
-                try {
-                    c = getContentResolver()
-                            .query(uri,
-                                    new String[]{
-                                            ContactsContract.Data.DISPLAY_NAME,
-                                            ContactsContract.Data.CONTACT_ID,
-                                            ContactsContract.CommonDataKinds.Phone.NUMBER,
-                                            ContactsContract.Contacts.PHOTO_ID},
-                                    null, null, null);
-
-                    if (c != null && c.moveToFirst()) {
-                        String name = c.getString(0);
-                        String id = c.getString(1);
-                        String number = c.getString(2);
-                        String photoId = c.getString(3);
-                        addPerson(name, Long.parseLong(id), number, photoId);
-                    }
-                } finally {
-                    if (c != null) {
-                        c.close();
-                    }
-                }
-            }
-        }
     }
 
     public void addPerson(String name, long id, String phoneNumber,
@@ -115,7 +79,7 @@ public class DashboardActivity extends AppCompatActivity {
                 Log.e("DashboardActivity", "Error on addSelectedPerson");
             }
 
-            dashboard.addView(new PersonView(person, this));
+            //dashboard.addView(new PersonView(person, this));
         } catch (Exception e1) {
             e1.printStackTrace();
         }
@@ -126,14 +90,13 @@ public class DashboardActivity extends AppCompatActivity {
         for (Person person : list) {
             person.setAmountInpirations(helper.getInspirationData(person.id)
                     .size());
-            dashboard.addView(new PersonView(person, this));
+            //dashboard.addView(new PersonView(person, this));
         }
         helper.close();
     }
 
     @Override
     protected void onResume() {
-        loadPersons();
         super.onResume();
     }
 
---
+++ PessoasInspiradoras/app/src/main/java/br/net/paulofernando/pessoasinspiradoras/view/fragment/PersonListFragment.java
@@ -0,0 +1,121 @@
+package br.net.paulofernando.pessoasinspiradoras.view.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+import br.net.paulofernando.pessoasinspiradoras.R;
+import br.net.paulofernando.pessoasinspiradoras.data.dao.DatabaseHelper;
+import br.net.paulofernando.pessoasinspiradoras.data.entity.Person;
+import br.net.paulofernando.pessoasinspiradoras.listener.EndlessRecyclerViewScrollListener;
+import br.net.paulofernando.pessoasinspiradoras.view.adapter.PersonAdapter;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class PersonListFragment extends Fragment {
+
+    private static final String TAG = "PersonListFragment";
+
+    @BindView(R.id.list_rv) RecyclerView mRecyclerView;
+
+    protected RecyclerView.LayoutManager mLayoutManager;
+    protected PersonAdapter mAdapter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+        //getData();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        ButterKnife.bind(this, rootView);
+
+        //mRecyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), R.drawable.divider));
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        setRecyclerViewLayoutManager();
+
+        getData();
+
+        return rootView;
+    }
+
+    /**
+     * Set RecyclerView's LayoutManager to the one given.
+     */
+    public void setRecyclerViewLayoutManager() {
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        positionScroll();
+        mRecyclerView.addOnScrollListener(new EndlessRecyclerViewScrollListener(mLayoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount) {
+                loadMoreData(page);
+            }
+        });
+    }
+
+    private void positionScroll() {
+        int scrollPosition = 0;
+        // If a layout manager has already been set, get current scroll position.
+        if (mRecyclerView.getLayoutManager() != null) {
+            scrollPosition = ((LinearLayoutManager) mRecyclerView.getLayoutManager())
+                    .findFirstCompletelyVisibleItemPosition();
+        }
+        mRecyclerView.scrollToPosition(scrollPosition);
+    }
+
+
+    public void loadMoreData(int page) {
+        Log.i(TAG, "Loading page " + (page + 1) + "...");
+        //loadingTextView.setVisibility(View.VISIBLE);
+        fillPeopleList(page + 1);
+    }
+
+    /** Retrieves the data from themoviedb */
+    protected void getData() {
+        fillPeopleList(1);
+    }
+
+    public void fillPeopleList(final int pageNumber) {
+        DatabaseHelper helper = new DatabaseHelper(this.getContext());
+
+        final List<Person> result = helper.getPersonsData();
+        if ((result != null) && (PersonListFragment.this.getActivity() != null)) {//there are movies to list
+            PersonListFragment.this.getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    updateList(result);
+                }
+            });
+        }
+
+        helper.close();
+    }
+
+    private void updateList(List<Person> result) {
+        if (mAdapter == null) {
+            mAdapter = new PersonAdapter(PersonListFragment.this.getContext());
+            mRecyclerView.setAdapter(mAdapter);
+            mAdapter.setItems(result);
+            mRecyclerView.setVisibility(View.VISIBLE);
+        } else {
+            mAdapter.addList(result);
+        }
+        //loadingTextView.setVisibility(View.GONE);
+    }
+
+
+}
