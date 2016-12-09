2ef782980f44eca1582f7a2537f156c9b0df05e4
paulofernando <pf@paulofernando.net.br>
2016-12-09 16:37:49 -0300

    Adding version in About Screen automatically.

--- PessoasInspiradoras/app/src/main/java/br/net/paulofernando/pessoasinspiradoras/view/activity/AboutActivity.java
+++ PessoasInspiradoras/app/src/main/java/br/net/paulofernando/pessoasinspiradoras/view/activity/AboutActivity.java
@@ -1,6 +1,8 @@
 package br.net.paulofernando.pessoasinspiradoras.view.activity;
 
 import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -8,13 +10,22 @@ import br.net.paulofernando.pessoasinspiradoras.R;
 
 public class AboutActivity extends Activity {
 
+    private static String version;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
+        try {
+            PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            version = "v. " + pInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
         TextView tvVersion = (TextView) findViewById(R.id.tv_version);
-        tvVersion.setText(getString(R.string.version) + " " + getString(R.string.version_number));
+        tvVersion.setText(getString(R.string.version) + " " + version);
     }
 
 }
